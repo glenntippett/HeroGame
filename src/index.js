b5c3cb4c94a1a4fd7605c24601b836d197f6ec7d
Glenn Tippett <glenn.tippett@gmail.com>
2021-05-04 16:52:57 +1000

    move logic of checking where hero is into own function

--- src/index.js
+++ src/index.js
@@ -11,24 +11,23 @@ document.addEventListener('DOMContentLoaded', () => {
     knight.setStartLocation()
   }
 
+  function checkIfHeroisNearAVillager () {
+    // Check if Hero is near the Knight
+    const knightSpeech = document.querySelector('.knight-speech')
+    hero.topLocation < 80 && (hero.leftLocation < 200 && hero.leftLocation > 50)
+      ? knightSpeech.style.display = 'block'
+      : knightSpeech.style.display = 'none'
+    // Check if Hero is near the Old Man
+    const oldManSpeech = document.querySelector('.old-man-speech')
+    hero.topLocation > 70 && hero.topLocation < 130 && (hero.leftLocation < 525 && hero.leftLocation > 415)
+      ? oldManSpeech.style.display = 'block'
+      : oldManSpeech.style.display = 'none'
+  }
+
   function moveHero (event) {
     if (event.code === 'Space') hero.attack()
     hero.moveHero(event)
-    // Check if Hero is near the Knight
-    const knightSpeech = document.querySelector('.knight-speech')
-    if (hero.topLocation < 80 && (hero.leftLocation < 200 && hero.leftLocation > 50)) {
-      knightSpeech.style.display = 'block'
-    } else {
-      knightSpeech.style.display = 'none'
-    }
-
-    // Check if Hero is near the Old Man
-    const oldManSpeech = document.querySelector('.old-man-speech')
-    if ((hero.topLocation > 70 && hero.topLocation < 130) && (hero.leftLocation < 525 && hero.leftLocation > 415)) {
-      oldManSpeech.style.display = 'block'
-    } else {
-      oldManSpeech.style.display = 'none'
-    }
+    checkIfHeroisNearAVillager()
   }
 
   document.addEventListener('keydown', moveHero)
